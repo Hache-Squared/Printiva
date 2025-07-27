409b0399d11d3bd716b388f23cffae3c47083076
Angel Gabriel Hernandez <angel.hernandez@pertendo.com>
2025-07-26 22:57:13 -0600

    Configuracion añadida para auto_scripts

---
+++ PrintivaDB/dbo/Scripts/Auto_Catalogs.sql
@@ -0,0 +1,22 @@
+﻿DECLARE @version VARCHAR(100) = '1.0.0';
+
+
+IF(@version = '1.0.0')
+BEGIN 
+	CREATE TABLE #TempCatalogoInventarioUnidades(
+		Nombre VARCHAR(200)
+	);
+
+	INSERT INTO #TempCatalogoInventarioUnidades(Nombre)
+	VALUES ('Unidad'),
+		   ('Gramos');
+
+	INSERT INTO dbo.TblInventariosTipos(Nombre)
+	SELECT temp.Nombre
+	FROM #TempCatalogoInventarioUnidades temp
+	LEFT JOIN dbo.TblInventariosTipos it (NOLOCK)
+		ON temp.Nombre = it.Nombre
+	WHERE it.Nombre IS NULL
+	
+	DROP TABLE IF EXISTS #TempCatalogoInventarioUnidades;
+END;
